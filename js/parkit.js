1ec999e55de33426f697bff6bec3c6065e78bcd8
Keisuke Todoroki <keisuke322+github@gmail.com>
2014-04-07 19:04:29 +0900

    #homeに文字時計表示JSを適用。独自アイコンはサイズ指定がまだ。

---
+++ js/parkit.js
@@ -0,0 +1,17 @@
+init();
+
+function init() {
+    // homeページがDOMに追加されてからの処理
+    $(document).delegate("#home", "pagecreate", displayDateTime);
+}
+
+function displayDateTime() {
+    var now = new Date();
+    var nowString = now.toLocaleDateString() + " " + now.toLocaleTimeString();
+
+    // homeページがDOMに追加されてからの処理
+    document.getElementById("timestampButton").innerHTML = nowString + "<br/>入庫";
+    
+    setTimeout(displayDateTime, 1000);
+}
+
