1fb9e588289257d6a5aeff1d66ece002248ea005
Keisuke Todoroki <keisuke322+github@gmail.com>
2014-04-16 14:46:24 +0900

    setTimerのコントロールなど

--- js/parkit.js
+++ js/parkit.js
@@ -1,8 +1,13 @@
 /*global $, console, init */
+/*jslint nomen: true */
 
-var storage = localStorage;
-var parkedDate = new Date();
-var parkingNumber = "";
+var _storage = localStorage;
+var _parkedDate = new Date(0);
+var _tempParkedDate = new Date(0);
+var _parkingNumber = "";
+var _refreshCurrentDateTimeTimer;
+var _refreshParkingTimeTimer;
+var _refreshParkInfoTimer;
 
 init();
 
@@ -22,16 +27,33 @@ function init() {
     $(document).delegate("#parkInfo", "pagecreate", displayParkInfoPage);
 }
 
-function setSessionStorage() {
+/*
+function setLocalStorage() {
     "use strict";
     
-    var key = parkedDate.getTime(),
+    var key = _tempParkedDate.getTime() == 0 ? _parkedDate.getTime() : _tempParkedDate.getTime(),
         value = document.getElementById("parkingNumberInput").value;
 
     if (key && value) {
-        storage.clear();
-        storage.setItem(key, value);
+        _storage.clear();
+        _storage.setItem(key, value);
     }
+}
+*/
+
+function setTempParkedDate() {
+    "use strict";
+    console.log("parked date has been changed.");
+
+    var ymd = document.getElementById("parkedDateInput").value,
+        hm = document.getElementById("parkedTimeInput").value;
+    _tempParkedDate = new Date(ymd + " " + hm + ":" + _parkedDate.getSeconds());
+}
+
+function getDate(yyyymmddString, hhmmString) {
+    "use strict";
+    
+    return new Date(dateString + " " + timeString);
 }
 
 function refreshCurrentDateTime() {
@@ -39,20 +61,20 @@ function refreshCurrentDateTime() {
 
     var now = new Date();
     document.getElementById("refreshCurrentDateTime").innerHTML = getFullYearMonthDate(now) + " " + getFullHourMinSec(now);
-    setTimeout(refreshCurrentDateTime, 1000);
+    _refreshCurrentDateTimeTimer = setTimeout(refreshCurrentDateTime, 1000);
 }
 
 function refreshParkingTime() {
     "use strict";
-    document.getElementById("parkingTimeForInput").innerHTML = getPassingHourMinSec(parkedDate);
-    setTimeout(refreshParkingTime, 1000);
+    document.getElementById("parkingTimeForInput").innerHTML = getPassingHourMinSec(_parkedDate);
+    _refreshParkingTimeTimer = setTimeout(refreshParkingTime, 1000);
 }
 
 function refreshParkInfo() {
     "use strict";
-    document.getElementById("parkingTime").innerHTML = getPassingHourMinSec(parkedDate);
-    document.getElementById("parkedTime").innerHTML = getFullYearMonthDate(parkedDate) + " " + getFullHourMinSec(parkedDate);
-    setTimeout(refreshParkInfo, 1000);
+    document.getElementById("parkingTime").innerHTML = getPassingHourMinSec(_parkedDate);
+    document.getElementById("parkedTime").innerHTML = getFullYearMonthDate(_parkedDate) + " " + getFullHourMinSec(_parkedDate);
+    _refreshParkInfoTimer = setTimeout(refreshParkInfo, 1000);
 }
 
 /**
@@ -62,7 +84,7 @@ function displayHomePage() {
     "use strict";
         
     /* 既に記録されたデータがあれば、駐車情報ページを表示させる */
-    if (storage.length > 0) {
+    if (_storage.length > 0) {
         $("body").pagecontainer("change", "#parkInfo");
     }
     
@@ -80,14 +102,14 @@ function displayParkInfoInputPage() {
     refreshParkingTime();
     
     /* 駐車番号をフォームにセットする */
-    if (parkingNumber === "" && storage.length > 0) {
-        parkingNumber = storage.getItem(storage.key(0));
+    if (_parkingNumber === "" && _storage.length > 0) {
+        _parkingNumber = _storage.getItem(_storage.key(0));
     }
-    document.getElementById("parkingNumberInput").value = parkingNumber;
+    document.getElementById("parkingNumberInput").value = _parkingNumber;
     
     /* 駐車時刻をフォームにセットする */
-    document.getElementById("parkedDateInput").value = getFullYearMonthDate(parkedDate);
-    document.getElementById("parkedTimeInput").value = getFullHourMin(parkedDate);
+    document.getElementById("parkedDateInput").value = getFullYearMonthDate(_parkedDate);
+    document.getElementById("parkedTimeInput").value = getFullHourMin(_parkedDate);
 }
 
 /**
@@ -95,13 +117,13 @@ function displayParkInfoInputPage() {
  */
 function displayParkInfoPage() {
     "use strict";
-    
+    console.log("displayParkInfoPage()");
     /* 駐車情報を表示する */
-    if (storage.length > 0) {
-        document.getElementById("parkingNumber").innerHTML = storage.getItem(storage.key(0));
-        if (parkedDate === null) {
-            var parkedTime = parseInt(storage.key(0), 10);
-            parkedDate = new Date(parkedTime);
+    if (_storage.length > 0) {
+        document.getElementById("parkingNumber").innerHTML = _storage.getItem(_storage.key(0));
+        if (_parkedDate.getTime() === 0) {
+            var parkedTime = parseInt(_storage.key(0), 10);
+            _parkedDate = new Date(parkedTime);
         }
         
         refreshParkInfo();
@@ -114,7 +136,9 @@ function displayParkInfoPage() {
 function submitPark() {
     "use strict";
     
-    parkedDate = new Date();
+    _parkedDate = new Date();
+    //clearTimeout(_refreshCurrentDateTimeTimer);
+    displayParkInfoInputPage();
 }
 
 /**
@@ -124,19 +148,48 @@ function submitParkInfo() {
     "use strict";
     
     /* Web Storageに記録する */
-    setSessionStorage();
+    //setLocalStorage();
+    _parkedDate = _tempParkedDate.getTime() == 0 ? _parkedDate : _tempParkedDate;
+    _parkingNumber = document.getElementById("parkingNumberInput").value;
+    
+    if (_parkedDate && _parkingNumber) {
+        _storage.clear();
+        _storage.setItem(_parkedDate.getTime(), _parkingNumber);
+    }
+    
     
     /* 駐車時刻アコーディオンを閉じる */
     $("#parkedDateTimeFieldSet").collapsible("option", "collapsed", true);
+    
+    clearTimeout(_refreshParkingTimeTimer);
+    displayParkInfoPage();
 }
 
 function submitParkFinish() {
     "use strict";
     
-    storage.clear();
-    parkedDate = null;
-    parkingNumber = "";
+    /* リセット */
+    _storage.clear();
+    _parkedDate = new Date(0);
+    _parkingNumber = "";
+    _tempParkedDate = new Date(0);
+    clearTimeout(_refreshParkInfoTimer);
     document.getElementById("parkingNumberInput").value = "";
+    
+    displayHomePage();
+}
+
+function editParkInfo() {
+    "use strict";
+    
+    displayParkInfoInputPage();
+}
+
+function cancelParkInfoInput() {
+    "use strict";
+    
+    _tempParkedDate = new Date(0);
+    //displayHomePage();
 }
 
 
